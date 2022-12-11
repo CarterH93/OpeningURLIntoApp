0be15f93696c729d63a7b20fbf05eb6c4e5246b3
CarterH93 <Carterhawkins93@gmail.com>
2022-12-10 23:08:33 -0500

    Initial Commit

---
+++ OpeningURLIntoApp/OpeningURLIntoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningURLIntoAppApp.swift
+//  OpeningURLIntoApp
+//
+//  Created by Carter Hawkins on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct OpeningURLIntoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
